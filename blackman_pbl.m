240601eb925114fdacefab46825b410225aa769f
Lucas Cardoso <skillinhow@users.noreply.github.com>
2022-11-02 23:04:43 -0300

    Mudanças feitas na sessão

--- blackman_pbl.m
+++ blackman_pbl.m
@@ -1,4 +1,4 @@
-function [w] = blackman_pbl(L)
+function [w, W] = blackman_pbl(L)
     N = L - 1;
     W = 0:N;
     w = 0.42 - 0.5 * cos(2*pi*W/N) + 0.08 * cos(4*pi*W/N);
--- hann_pbl.m
+++ hann_pbl.m
@@ -1,5 +1,6 @@
-function [w] = hann_pbl(L)
+function [w, W] = hann_pbl(L)
     N = L - 1;
     W = 0:N;
-    w = 0.5 - 0.5 * cos(2*pi*W/N);
+%     w = 0.5 - 0.5 * cos(2*pi*W/N);
+    w = 0.5 * (1 - cos(2*pi*W/N));
 end
